966ee1d7611d5222de102663a58c2cf01538255f
Joao Valverde <acci.valverde@gmail.com>
2018-11-05 00:18:10 +0000

    allow authorization

--- core/utils/middleware.py
+++ core/utils/middleware.py
@@ -5,6 +5,6 @@ class CORSMiddleware(object):
     def __call__(self, request):
         response = self.get_response(request)
         response['Access-Control-Allow-Origin'] = '*'
-        response['Access-Control-Allow-Headers'] = 'content-type'
+        response['Access-Control-Allow-Headers'] = 'content-type, authorization'
 
         return response
