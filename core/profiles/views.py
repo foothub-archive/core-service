8ffb08ae8a8e30357ad5a3d734daa75bc0e61b0c
Joao Valverde <acci.valverde@gmail.com>
2018-11-20 22:14:32 +0000

    working on profiles

--- core/profiles/views.py
+++ core/profiles/views.py
@@ -1,21 +1,23 @@
-from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin, ListModelMixin
+from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, ListModelMixin
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.filters import SearchFilter
-from rest_framework.decorators import list_route
+from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .models import Profile
-from .serializers import ProfileSerializer
+from .serializers import ProfileSerializer, CreateProfileSerializer
 from .permissions import ProfilePermissions
 
 
-class ProfileViewSet(RetrieveModelMixin,
+class ProfileViewSet(CreateModelMixin,
+                     RetrieveModelMixin,
                      UpdateModelMixin,
                      ListModelMixin,
                      GenericViewSet):
 
     model_class = Profile
     serializer_class = ProfileSerializer
+    create_serializer_class = CreateProfileSerializer
     permission_classes = (ProfilePermissions,)
     filter_backends = (SearchFilter,)
     search_fields = ('name',)
@@ -23,7 +25,13 @@ class ProfileViewSet(RetrieveModelMixin,
     def get_queryset(self):
         return self.model_class.objects.all()
 
-    @list_route(methods=['get'])
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return self.create_serializer_class
+
+        return self.serializer_class
+
+    @action(methods=['get'], detail=False)
     def me(self, request, *args, **kwargs):
         instance = request.user
         serializer = self.get_serializer(instance)
