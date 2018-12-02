216b74503e7765478daa170b84d420ec6f151a29
Joao Valverde <acci.valverde@gmail.com>
2018-12-02 17:59:23 +0000

    support for invite creation

--- core/friendships/serializers.py
+++ core/friendships/serializers.py
@@ -1,24 +1,40 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 
 from profiles.serializers import ProfileSerializer
+from profiles.models import Profile
 
 from .models import FriendshipInvitation
-
-
-class FriendshipInvitationSerializerMeta:
-    model = FriendshipInvitation
-    fields = ('id', 'friend')
 
 
 class ReceivedFriendshipInvitationSerializer(serializers.ModelSerializer):
     friend = ProfileSerializer(many=False, read_only=True, source='inviting')
 
-    class Meta(FriendshipInvitationSerializerMeta):
-        pass
+    class Meta:
+        model = FriendshipInvitation
+        fields = ('id', 'friend')
 
 
 class CreatedFriendshipInvitationSerializer(serializers.ModelSerializer):
     friend = ProfileSerializer(many=False, read_only=True, source='invited')
+    friend_uuid = serializers.UUIDField(write_only=True, format='hex')
 
-    class Meta(FriendshipInvitationSerializerMeta):
-        pass
+    def validate(self, data):
+        try:
+            invited = Profile.objects.get(external_uuid=data['friend_uuid'].hex)
+        except ObjectDoesNotExist:
+            raise serializers.ValidationError("Unable to find Invited Profile.")
+
+        inviting = self.context['request'].user
+
+        return {
+            'inviting': inviting,
+            'invited': invited,
+        }
+
+    def create(self, validated_data):
+        return self.Meta.model.objects.create(**validated_data)
+
+    class Meta:
+        model = FriendshipInvitation
+        fields = ('id', 'friend', 'friend_uuid')
--- core/friendships/test.py
+++ core/friendships/test.py
@@ -216,31 +216,54 @@ class TestCreatedFriendshipInvitationsApi(APITestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json()['friend']['uuid'], self.vasco.uuid)
 
-    """
-    def test_create_400(self):
+    def test_create_40_required(self):
         response = self.client.post(
             self.URL, data=json.dumps({}), content_type=self.CONTENT_TYPE, **self.http_auth)
         self.assertEqual(response.status_code, 400)
-        self.assertIn('invited', response.json())
-        self.assertEqual(response.json()['invited'], ["This field is required."])
+        self.assertIn('friend_uuid', response.json())
+        self.assertEqual(response.json()['friend_uuid'], ["This field is required."])
 
-    def test_create_201(self):
-        payload = {
-            'invited': self.chi.uuid,
-        }
-
-        self.assertEqual(
-            FriendshipInvitation.objects.filter(inviting=self.joao.uuid).count(), 1)
+    def test_create_400_invalid(self):
+        payload = {'friend_uuid': 'abc'}
 
         response = self.client.post(
-            self.URL, data=json.dumps(payload), content_type=self.CONTENT_TYPE)
+            self.URL, data=json.dumps(payload), content_type=self.CONTENT_TYPE, **self.http_auth)
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('friend_uuid', response.json())
+        self.assertEqual(response.json()['friend_uuid'], ["Must be a valid UUID."])
+
+    def test_create_400_no_profile(self):
+        payload = {'friend_uuid': self.chi.uuid}
+        self.chi.delete()
+
+        response = self.client.post(
+            self.URL, data=json.dumps(payload), content_type=self.CONTENT_TYPE, **self.http_auth)
+        self.assertEqual(response.status_code, 400)
+        self.assertIn('non_field_errors', response.json())
+        self.assertEqual(response.json()['non_field_errors'], ["Unable to find Invited Profile."])
+
+    def test_create_201(self):
+        payload = {'friend_uuid': self.chi.uuid}
+
+        from profiles.models import Profile
+        Profile.objects.get(external_uuid=payload['friend_uuid'])
+
+        self.assertEqual(FriendshipInvitation.objects.filter(inviting=self.joao).count(), 1)
+
+        response = self.client.post(
+            self.URL, data=json.dumps(payload), content_type=self.CONTENT_TYPE, **self.http_auth)
+        print('\n\n\n~~~~~')
+        import pprint
+        pprint.pprint(response.json())
+        print('~~~~~\n\n\n')
         self.assertEqual(response.status_code, 201)
         self.assertEqual(
-            FriendshipInvitation.objects.filter(inviting=self.joao.uuid).count(), 2)
+            FriendshipInvitation.objects.filter(inviting=self.joao).count(), 2)
 
-        self.assertEqual(
-            FriendshipInvitation.objects.get(id=response.json()['id']).invited, self.chi)
-    """
+        new_invitation = FriendshipInvitation.objects.get(id=response.json()['id'])
+        self.assertEqual(new_invitation.inviting, self.joao)
+        self.assertEqual(new_invitation.invited, self.chi)
+
     def test_update_405(self):
         response = self.client.put(self.instance_url(self.joao_vasco_inivitation),
                                    data=json.dumps({}), content_type=self.CONTENT_TYPE, **self.http_auth)
